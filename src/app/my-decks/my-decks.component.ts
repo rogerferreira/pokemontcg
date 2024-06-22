645b11e8f6008067d967bfd8ba45d1c6259793d3
Roger Ferreira <rogerdarochaferreira@gmail.com>
2024-06-22 19:45:53 -0300

    criado function remover, remover todos descks

--- src/app/my-decks/my-decks.component.ts
+++ src/app/my-decks/my-decks.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Decks} from "../interfaces/decks.interface";
 import {PokemonTcgService} from "../services/api/pokemon-tcg.service";
 import {DecksService} from "../services/decks.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-my-decks',
@@ -13,11 +14,23 @@ export class MyDecksComponent {
   public decks: Decks[] = this.decksService.getDecks();
   public deck: Decks = this.decksService.getDeck(0);
 
-  constructor(private api: PokemonTcgService,
+  constructor(private router: Router,
+              private api: PokemonTcgService,
               private decksService: DecksService) {
   }
 
   public buscar(id: number): void{
     this.deck = this.decksService.getDeck(id);
+  }
+
+  public async removeDeck(id: number): Promise<void>{
+    this.decks = this.decksService.removeDeck(id, this.decks);
+    await this.decksService.createDeck(this.decks);
+    this.deck = this.decksService.getDeck(0);
+  }
+
+  public async dropDecks(): Promise<void>{
+    await this.decksService.dropDeck();
+    await this.router.navigate(['']);
   }
 }
